de8e5b3b2849bec3ba180553d30db01e62070c59
Trần Quang Đức <47981725+ductq1999@users.noreply.github.com>
2020-12-02 22:30:57 +0700

    Update driverList.vue

--- pages/driver/driverList.vue
+++ pages/driver/driverList.vue
@@ -1,120 +1,143 @@
 <template>
   <div class="row">
-    <div class="col-md-12">
-      <card card-body-classes="table-full-width">
-        <h4 slot="header" class="card-title">Driver</h4>
-        <nuxt-link to="add"
-          ><i class="tim-icons icon-simple-add"></i> Thêm tài xế</nuxt-link
-        >
-        <!-- <form>
-          <div class="form-row">
-            <div class="form-group col-md-6">
-              <label>Họ tên</label>
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Nhập họ tên"
-                v-model="driver.name"
-              />
+    <no-ssr>
+      <div class="col-md-12">
+        <card card-body-classes="table-full-width">
+          <h4 slot="header" class="card-title">Driver</h4>
+          <nuxt-link to="add"
+            ><i class="tim-icons icon-simple-add"></i> Thêm tài xế</nuxt-link
+          >
+          <form method="get">
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label>Họ tên</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Nhập họ tên"
+                  v-model="driver.name"
+                />
+              </div>
+              <div class="form-group col-md-6">
+                <label>Số CMTND</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Nhập số CMTND"
+                  v-model="driver.idNumber"
+                />
+              </div>
             </div>
-            <div class="form-group col-md-6">
-              <label>Số CMTND</label>
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Nhập số CMTND"
-                v-model="driver.idNumber"
-              />
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label>Loại bằng lái</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Nhập loại bằng lái"
+                  v-model="driver.licenseType"
+                />
+              </div>
+              <div class="form-group col-md-6">
+                <label>Địa chỉ</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  placeholder="Nhập địa chỉ"
+                  v-model="driver.address"
+                />
+              </div>
             </div>
-          </div>
-          <div class="form-row">
-            <div class="form-group col-md-6">
-              <label>Loại bằng lái</label>
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Nhập loại bằng lái"
-                v-model="driver.licenseType"
-              />
+            <div class="form-row">
+              <div class="form-group col-md-6">
+                <label>Sắp xếp theo</label>
+                <select class="form-control" v-model="columnSortName">
+                  <option style="color: black" value="" selected>Mặc định</option>
+                  <option style="color: black" value="name">Tên</option>
+                  <option style="color: black" value="idNumber">Số chứng minh thư</option>
+                  <option style="color: black" value="address">Địa chỉ</option>
+                  <option style="color: black" value="licenseType">Loại GPLX</option>
+                </select>
+              </div>
+              <div class="form-group col-md-6">
+                <label>Thứ tự</label>
+                <select class="form-control" v-model="asc">
+                  <option style="color: black" value="true">Từ A -> Z</option>
+                  <option style="color: black" value="false">Từ Z -> A</option>
+                </select>
+              </div>
             </div>
-            <div class="form-group col-md-6">
-              <label>Địa chỉ</label>
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Nhập địa chỉ"
-                v-model="driver.address"
-              />
-            </div>
-          </div>
-          <button class="btn btn-primary" @click="getDriverByCondition">
-            Tìm kiếm
-          </button>
-        </form> -->
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">Tên</th>
-              <th scope="col">Ngày sinh</th>
-              <th scope="col">Số CMTNN</th>
-              <th scope="col">Địa chỉ</th>
-              <th scope="col">Số GPLX</th>
-              <th scope="col">Loại GPLX</th>
-              <th scope="col">Năm kinh nghiệm</th>
-              <th scope="col" style="text-align: center">Hành động</th>
-            </tr>
-          </thead>
-          <tbody v-if="allDriver">
-            <tr v-for="(driver, index) in driverByCondition" :key="index">
-              <td>{{ driver.name }}</td>
-              <td>{{ formatDate(driver.dateOfBirth) }}</td>
-              <td>{{ driver.idNumber }}</td>
-              <td>{{ driver.address }}</td>
-              <td>{{ driver.licenseNumber }}</td>
-              <td>{{ driver.licenseType }}</td>
-              <td>
-                <div v-if="driver.seniority > 0">
-                  {{ driver.seniority }} năm
-                </div>
-                <div v-else>Chưa có kinh nghiệm</div>
-              </td>
-              <td style="text-align: center">
-                <nuxt-link :to="{ path: '/driver/' + driver.id }"
-                  ><i class="tim-icons icon-pencil"></i
-                ></nuxt-link>
-                <button @click="showModal(driver.id)">
-                  <i class="tim-icons icon-trash-simple"></i>
-                </button>
-              </td>
-            </tr>
-          </tbody>
-          <tbody v-else>
-            Không có bản ghi
-          </tbody>
-        </table>
-      </card>
-      <b-pagination
-          v-if="rows > pageSize"
+            <a class="btn btn-primary" @click="getDriverByCondition">
+              Tìm kiếm
+            </a>
+          </form>
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">Tên</th>
+                <th scope="col">Ngày sinh</th>
+                <th scope="col">Số CMTNN</th>
+                <th scope="col">Địa chỉ</th>
+                <th scope="col">Số GPLX</th>
+                <th scope="col">Loại GPLX</th>
+                <th scope="col">Năm kinh nghiệm</th>
+                <th scope="col" style="text-align: center">Hành động</th>
+              </tr>
+            </thead>
+            <tbody v-if="allDriver">
+              <tr v-for="(driver, index) in driverByCondition" :key="index">
+                <td>{{ driver.name }}</td>
+                <td>{{ formatDate(driver.dateOfBirth) }}</td>
+                <td>{{ driver.idNumber }}</td>
+                <td>{{ driver.address }}</td>
+                <td>{{ driver.licenseNumber }}</td>
+                <td>{{ driver.licenseType }}</td>
+                <td>
+                  <div v-if="driver.seniority > 0">
+                    {{ driver.seniority }} năm
+                  </div>
+                  <div v-else>Chưa có kinh nghiệm</div>
+                </td>
+                <td style="text-align: center">
+                  <nuxt-link :to="{ path: '/driver/' + driver.id }"
+                    ><i class="tim-icons icon-pencil"></i
+                  ></nuxt-link>
+                  <a style="cursor: pointer" @click="showModal(driver.id)">
+                    <i class="tim-icons icon-trash-simple"></i>
+                  </a>
+                </td>
+              </tr>
+            </tbody>
+            <tbody v-else>
+              Không có bản ghi
+            </tbody>
+          </table>
+        </card>
+        <b-pagination
           v-model="currentPage"
           :total-rows="rows"
           :per-page="pageSize"
         ></b-pagination>
-    </div>
-    <b-modal ref="my-modal" id="modal-scoped">
-      <div>Bạn có chắc chắn muốn xóa tài xế này không?</div>
-      <template v-slot:modal-footer="{ cancel }">
-        <b-button size="sm" variant="success" @click="deleteDriver(idd)"
-          >Đồng ý</b-button
-        >
-        <b-button size="sm" variant="danger" @click="cancel">Hủy bỏ</b-button>
-      </template>
-    </b-modal>
+      </div>
+      <b-modal ref="my-modal" id="modal-scoped">
+        <div>Bạn có chắc chắn muốn xóa tài xế này không?</div>
+        <template v-slot:modal-footer="{ cancel }">
+          <b-button size="sm" variant="success" @click="deleteDriver(idd)"
+            >Đồng ý</b-button
+          >
+          <b-button size="sm" variant="danger" @click="cancel">Hủy bỏ</b-button>
+        </template>
+      </b-modal>
+    </no-ssr>
   </div>
 </template>
 <script>
 import { mapGetters, mapState } from "vuex";
-
+import NoSSR from "vue-no-ssr";
 export default {
+  components: {
+    "no-ssr": NoSSR,
+  },
   data() {
     return {
       idd: null,
@@ -128,10 +151,10 @@ export default {
         dateOfBirth: "",
         seniority: "",
         idNumber: "",
-        status: "",
+        status: 1,
       },
       columnSortName: "",
-      asc: true,
+      asc: "",
     };
   },
   watch: {
@@ -140,6 +163,30 @@ export default {
       this.currentPage = val;
       this.getDriverByCondition();
     },
+    // asc(val) {
+    //   this.asc = val;
+    //   this.getDriverByCondition();
+    // },
+    // columnSortName(val) {
+    //   this.columnSortName = val;
+    //   this.getDriverByCondition();
+    // },
+    name(val) {
+      this.driver.name = val;
+      this.getDriverByCondition();
+    },
+    // idNumber(val){
+    //   this.driver.idNumber = val
+    //   this.getDriverByCondition()
+    // },
+    // licenseType(val){
+    //   this.driver.licenseType = val
+    //   this.getDriverByCondition()
+    // },
+    // address(val){
+    //   this.driver.address = val
+    //   this.getDriverByCondition()
+    // }
   },
   computed: {
     ...mapGetters({
@@ -151,7 +198,7 @@ export default {
     }),
   },
   mounted() {
-    this.getDriver();
+    // this.getDriver();
     this.getDriverByCondition();
   },
   methods: {
